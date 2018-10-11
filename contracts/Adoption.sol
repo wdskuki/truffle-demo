bc787038f464fa0a1c1cbd8a30ffa93c76bc22dc
Dongsheng Wei <weidongsheng@dajiazhongyi.com>
2018-10-11 18:40:31 +0800

    添加pet shop project

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -1,20 +1,17 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.24;
 
 contract Adoption {
+  address[16] public adopters;
 
-  address[16] public adopters;  // 保存领养者的地址
-
-    // 领养宠物
   function adopt(uint petId) public returns (uint) {
-    require(petId >= 0 && petId <= 15);  // 确保id在数组长度内
+    require(petId >= 0 && petId <=15);
 
-    adopters[petId] = msg.sender;        // 保存调用这地址
+    adopters[petId] = msg.sender;
+
     return petId;
   }
 
-  // 返回领养者
-  function getAdopters() public view returns (address[16]) {
+  function getAdopters() public view returns (address[16] ) {
     return adopters;
   }
-
 }
